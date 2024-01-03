7d5cfd88ab2920be668e3f070cb4f209e30d89c7
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-01-03 10:50:46 +0100

    TP 1 Test unitaire pas fonctionnel a 100%

--- src/test/java/cda/java/Palindrome/PalindromeTest.java
+++ src/test/java/cda/java/Palindrome/PalindromeTest.java
@@ -1,2 +1,35 @@
-package cda.java.Palindrome;public class PalindromeTest {
+package cda.java.Palindrome;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import cda.java.exercices.palindrome.Palindrome;
+public class PalindromeTest {
+
+    @Test
+    public void testIsPalindromeWithPalindromeNumber() {
+        assertTrue(Palindrome.isPalindrome(1221));
+    }
+
+    @Test
+    public void testIsPalindromeWithNonPalindromeNumber() {
+        assertFalse(Palindrome.isPalindrome(1234));
+    }
+
+    @Test
+    public void testIsPalindromeWithSingleDigitNumber() {
+        assertTrue(Palindrome.isPalindrome(5));
+    }
+
+    @Test
+    public void testIsPalindromeWithZero() {
+        assertTrue(Palindrome.isPalindrome(0));
+    }
+
+    @Test
+    public void testIsPalindromeWithNegativeNumber() {
+        assertFalse(Palindrome.isPalindrome(-121));
+    }
+
+    // Vous pouvez ajouter d'autres tests selon les cas de votre application.
+
 }
--- src/test/java/cda/java/RomainToArabeTest/RomainToArabeTest.java
+++ src/test/java/cda/java/RomainToArabeTest/RomainToArabeTest.java
@@ -1,2 +1,46 @@
-package cda.java.RomainToArabeTest;public class RomainToArabeTest {
+package cda.java.RomainToArabeTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import cda.java.exercices.RomainToArabe.RomainToArabe;
+public class RomainToArabeTest {
+
+    @Test
+    public void testEstNombreRomainValideWithValidRomanNumber() {
+        assertTrue(RomainToArabe.estNombreRomainValide("XIV"));
+    }
+
+    @Test
+    public void testEstNombreRomainValideWithInvalidRomanNumber() {
+        assertFalse(RomainToArabe.estNombreRomainValide("ABC"));
+    }
+
+    @Test
+    public void testEstNombreRomainValideWithEmptyString() {
+        assertFalse(RomainToArabe.estNombreRomainValide(""));
+    }
+
+    @Test
+    public void testEstNombreRomainValideWithTooLongString() {
+        assertFalse(RomainToArabe.estNombreRomainValide("MMMMMMMMMMMMMMM"));
+    }
+
+    @Test
+    public void testConvertirRomanEnArabeWithValidRomanNumber() {
+        assertEquals(14, RomainToArabe.convertirRomanEnArabe("XIV"));
+    }
+
+    @Test
+    public void testConvertirRomanEnArabeWithInvalidRomanNumber() {
+        assertEquals(0, RomainToArabe.convertirRomanEnArabe("ABC"));
+    }
+
+    @Test
+    public void testConvertirRomanEnArabeWithEmptyString() {
+        assertEquals(0, RomainToArabe.convertirRomanEnArabe(""));
+    }
+
+    // Vous pouvez ajouter d'autres tests selon les cas de votre application.
+
 }
