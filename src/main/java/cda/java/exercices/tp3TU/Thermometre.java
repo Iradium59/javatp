04ac403195fc1d5c79d68efd26fe2bc6a76d5286
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-01-03 11:32:41 +0100

    feat : TemperatureProcheDeZeroAvecNegatif :white_check_mark:

--- src/main/java/cda/java/exercices/tp3TU/Thermometre.java
+++ src/main/java/cda/java/exercices/tp3TU/Thermometre.java
@@ -2,6 +2,23 @@ package cda.java.exercices.tp3TU;
 
 public class Thermometre {
     public int getTemperatureProcheDeZero(int[] temperatures) {
-        return 0;
+        if (temperatures.length > 10000) {
+            throw new IllegalArgumentException("Trop de mesures, veuillez en fournir moins de 10000.");
+        }
+
+        if (temperatures.length == 0) {
+            return 0;
+        }
+
+        int temperatureProcheDeZero = temperatures[0];
+
+        for (int temperature : temperatures) {
+            if (Math.abs(temperature) < Math.abs(temperatureProcheDeZero) ||
+                    (Math.abs(temperature) == Math.abs(temperatureProcheDeZero) && temperature > 0)) {
+                temperatureProcheDeZero = temperature;
+            }
+        }
+
+        return temperatureProcheDeZero;
     }
 }
