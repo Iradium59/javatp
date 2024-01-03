21310d80d3aff5ae1246a7c659d5860765e60258
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-01-03 12:00:14 +0100

    test : push and pop :white_check_mark:

--- src/main/java/cda/java/exercices/tp3TU/Stack.java
+++ src/main/java/cda/java/exercices/tp3TU/Stack.java
@@ -1,40 +1,46 @@
 package cda.java.exercices.tp3TU;
 
 public class Stack {
-    private static final int MAX_SIZE = 10;
+
     private int[] elements;
-    private int size;
+    private int top;
 
     public Stack() {
-        elements = new int[MAX_SIZE];
-        size = 0;
+        this.elements = new int[10];
     }
 
-    public void push(int value) {
-        if (size < MAX_SIZE) {
-            elements[size++] = value;
+    public void push(int element) {
+        if (top < elements.length - 1) {
+            top++;
+            elements[top] = element;
+        } else {
+            throw new IllegalStateException("La stack est pleine");
         }
     }
 
     public int pop() {
-        if (size > 0) {
-            return elements[--size];
+        if (!isEmpty()) {
+            int poppedElement = elements[top];
+            top--;
+            return poppedElement;
+        } else {
+            throw new IllegalStateException("La stack est vide");
         }
-        return 0; // Valeur arbitraire à définir selon vos besoins
     }
 
     public int peek() {
-        if (size > 0) {
-            return elements[size - 1];
+        if (!isEmpty()) {
+            return elements[top];
+        } else {
+            throw new IllegalStateException("La stack est vide");
         }
-        return 0; // Valeur arbitraire à définir selon vos besoins
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return top == -1;
     }
 
     public boolean isFull() {
-        return size == MAX_SIZE;
+        return top == elements.length - 1;
     }
 }
